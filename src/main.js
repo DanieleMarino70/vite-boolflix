e3040c46427307e3147f09ef9aa7f8eb11657a2f
DanieleMarino70 <danielaotitan@gmail.com>
2023-03-03 17:07:35 +0100

    init

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import { createApp } from 'vue'
+
+//import bootstrap css
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import App from './App.vue'
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faStar)
+
+createApp(App).mount('#app')
+
+
+import "bootstrap/dist/js/bootstrap.js"
